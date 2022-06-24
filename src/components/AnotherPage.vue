368a1b1de433ee968a687f6395d4279cad78f038
meetfemisamuel <meetfemisamuel@yahoo.com>
2022-06-24 15:39:03 +0100

    v-for loop for listing

---
+++ src/components/AnotherPage.vue
@@ -0,0 +1,36 @@
+<template>
+
+  <div class="tw-items-center">
+    Another Page
+
+    <div>
+      <h5 class="instruction tw-mt-16 tw-mb-5">Blah Instruction</h5>
+
+      <ul v-for="(instruction,index) in instructions" :key="index">
+        <li>{{ instruction }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AnotherPage",
+  data() {
+    return {
+      instructions: {
+        first_instruction: 'Make sure you have a stable internet connection before you start.',
+        second_instruction: 'Once you begin, you cannot go back as you would be forced to submit.',
+        third_instruction: 'Once you have submitted an answer, you can’t go back.',
+        fourth_instruction: 'You may exit the test, but the timer will continue to run.',
+        fifth_instruction: 'You will see a time notice when you have 10 minutes left for the test.',
+        sixth_instruction: 'If the test can be retaken, your latest score will be the score used.'
+      },
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <Testing/>
+
+    <v-row no-gutters>
+      <v-col cols="12" md="2"></v-col>
+    <v-btn @click="naviGate">Navigate</v-btn>
+    </v-row>
+
+    <About :openDialog="dialog" @close="dialog=false"/>
+  </div>
+
+</template>
+
+<script>
+// @ is an alias to /src
+import Testing from '@/components/Testing.vue'
+import About from "@/views/About";
+
+export default {
+  name: 'Home',
+  components: {
+    Testing,
+    About,
+  },
+  methods: {
+    naviGate(){
+      this.$router.push({name:'Testing'})
+    }
+  }
+}
+</script>
