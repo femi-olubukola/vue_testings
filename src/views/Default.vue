ab3e3e1906e2565241de6dfcb1e8d2c16b0cbb20
meetfemisamuel <meetfemisamuel@yahoo.com>
2022-08-19 16:46:13 +0100

    default page and expansion panel added

---
+++ src/views/Default.vue
@@ -0,0 +1,33 @@
+<template>
+
+  <div id="app">
+    <button @click="increase">Click Me</button>
+    <p>{{counter}}</p>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "Default",
+  data() {
+    return {
+      counter: 0,
+      message: "hallos",
+    }
+  },
+  methods: {
+    increase() {
+      this.counter ++
+    },
+  }
+}
+</script>
+
+<style scoped>
+button {
+  width: 200px;
+  height: 30px;
+}
+
+</style>
